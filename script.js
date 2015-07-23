1834f233fc1c092410e048dd85712af8287c6be0
Mashon Thomas <mashonthomas@yahoo.com>
2015-07-23 18:36:02 -0400

    Complete forecast lesson

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var main = function() {
-  
+  $(".day").click(function(){
+    $(this).next().toggle();
+      $(this).find(".glyphicon.glyphicon-plus").toggleClass("glyphicon-minus");
+
+  });
 };
 
 $(document).ready(main);
